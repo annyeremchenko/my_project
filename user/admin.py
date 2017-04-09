e424e4be7bede852fbe1a27c6b5aba0457f9ae67
annyeremchenko <a.yeremchenko@gmail.com>
2017-04-09 12:13:46 +0600

    start html and css

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserInfo, Location
+
+admin.site.register(UserInfo)
+admin.site.register(Location)
 
 # Register your models here.
--- user/models.py
+++ user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Location(models.Model):
+    x = models.FloatField()
+    y = models.FloatField()
+    info = models.TextField(null=True)
+
+    def dict(self):
+        return {"x": self.x,
+                "y": self.y}
+
+
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, related_name="info")
+    points = models.IntegerField(default=100)
+    location = models.ForeignKey(Location, null=True, related_name="user")
+
