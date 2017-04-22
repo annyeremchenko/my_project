a8d42885a18a93f1ea7b26dfedea1fc13a2cdc3f
may420 <may.polikarpova@gmail.com>
2017-04-22 17:11:36 +0600

    make it better

--- kursach/settings.py
+++ kursach/settings.py
@@ -128,3 +128,4 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
